5534ecf70fcc666a15ab5d3b841dec5a0f558b78
ibruma-ro <bruma.ionut@gmail.com>
2026-01-30 01:00:24 +0200

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write("The name of smoothie will be:", name_on_order)
 
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SERACH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
